5251e896734c9b429086d81a7407a3837ccb1c5d
Andrew <echowhite93@yahoo.com>
2022-10-05 17:09:32 -0700

    created all mutations using apollo server

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,83 @@
+import { gql } from '@apollo/client';
+
+export const REMOVE_BOOK = gql`
+mutation DeleteBook($bookId: ID!) {
+    deleteBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        image
+        link
+        title
+      }
+    }
+  }
+`
+
+export const LOGIN_USER = gql`
+mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          authors
+          description
+          image
+          link
+          title
+        }
+      }
+    }
+  }
+`
+
+export const ADD_USER = gql`
+mutation AddUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          authors
+          description
+          image
+          link
+          title
+        }
+      }
+    }
+  }
+`
+
+export const SAVE_BOOK = gql`
+mutation SaveBook($bookData: BookInput!) {
+    saveBook(bookData: $bookData) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        image
+        link
+        title
+      }
+    }
+  }
+`
